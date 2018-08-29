fdeb8201c881a96e379fe171644b706035069270
Niall Tyndall <niall.tyndall@cyberlytic.com>
2018-08-29 17:29:36 +0100

    Update data sourcing

---
+++ R/data.R
@@ -0,0 +1,122 @@
+#' @title Data
+#'
+#' @export
+
+data <- function(pkg = ".") {
+
+  # Read the correct package
+  pkg <- if (pkg == ".") {
+    getwd() %>%
+      strsplit(split = "/") %>%
+      purrr::flatten_chr() %>%
+      utils::tail(1)
+  } else {
+    pkg
+  }
+
+  # Try and load the package
+  loaded <- require(pkg, character.only = TRUE)
+  if (!loaded) stop(" ## Package is not installed, try installing first")
+
+  # Check what data sets are available
+  myData <- utils::data(package = pkg) %>%
+    `[[`("results")
+
+  # Get all the training data set names
+  allDataNames <- myData[ , 'Item']
+  dLen <- allDataNames %>% length
+  allTitles <- myData[ , 'Title']
+
+  prnt_head <- function(...) paste0("  ", crayon::bgBlack(paste(..., collapse = "")), "\n")
+
+  cat("\n")
+  cat(prnt_head("## Available data sets"))
+  cat(prnt_head("## For newly created data sets you may need to document and rebuild with devtools!"))
+  cat("\n")
+
+  # Create full data set here
+  dataList <- c()
+  if (dLen %>% `>`(0)) {
+    dataList %<>% c(prnt_head("## Summary:", dLen, "data sets found."))
+    for (i in 1:dLen) {
+      # See if any documentation exists
+      txt <- tryCatch(
+        expr = gbRd::Rd_help2txt(rdName, pkgname = package),
+        error = function(e) NULL
+      )
+
+      # Chose the correct symbol
+      symbol <- if (txt %>% is.null) {
+        crayon::red(clisymbols::symbol$cross)
+      } else {
+        crayon::green(clisymbols::symbol$tick)
+      }
+
+      # Append text on
+      dataList %<>% c(
+        paste0(
+          " ", clisymbols::symbol$circle_filled, '  ', allDataNames[i] , '\n',
+          "     ", clisymbols::symbol$arrow_right, '  Docs : ', symbol, '\n',
+          "     ", clisymbols::symbol$arrow_right, '  Title : `', allTitles[i], '`\n'
+        )
+      )
+    }
+  } else {
+    dataList %<>% c("0 data sets found.")
+  }
+
+  dataList %<>% c('\n')
+  dataList <- c('', dataList)
+  cat(dataList)
+
+  # Print further documentation for info on data set
+  investigate <- TRUE
+  titlePattern <- '_\b'
+  while (investigate) {
+    cat(' \n\n ## Allowed data names : \n')
+    cat('', clisymbols::symbol$circle_filled, '',
+        allDataNames %>% paste(collapse = paste0('  \n ', clisymbols::symbol$circle_filled, '  ')))
+    cat(paste0(' \n\n ## Choose a data set, i.e. `', allDataNames[1], '` (or type q to quit) \n'))
+    cat(' ## Data set : ')
+    newTxt <- c()
+    dataSet <- readLines(con = "stdin", n = 1)
+
+    # Check if the data input is in the allowed names
+    if (dataSet %in% allDataNames) {
+      # See if the documentation exists...
+      helpTxt  <- tryCatch({
+        gbRd::Rd_help2txt(dataSet, pkgname = 'research')
+      }, error = function(e) NULL)
+
+      if (`!`(helpTxt %>% is.null)) {
+        for (j in 1:(helpTxt %>% length)) {
+          title <- helpTxt[j] %>% grepl(pattern = titlePattern)
+          if (title) {
+            newTxt %<>% c(
+              helpTxt[j] %>%
+                strsplit(split = titlePattern) %>%
+                purrr::flatten_chr() %>%
+                paste(collapse = '') %>%
+                paste0('\n')
+            )
+          } else {
+            newTxt %<>% c(paste0(helpTxt[j], '\n'))
+          }
+        }
+      } else {
+        cat(paste0(' ## ', dataSet, ' has no documentation... \n'))
+      }
+    } else {
+      if (dataSet %>% `==`('q')) {
+        investigate <- FALSE
+      } else {
+        cat(paste0(' ## ', dataSet, ' does not exist... \n'))
+      }
+    }
+
+    # Print out results of gbRd
+    cat(newTxt)
+  }
+
+  cat('\n  ## Complete.\n')
+}
